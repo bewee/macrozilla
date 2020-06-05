a8f156cb296abc39ab9510b192dc0bfae40e6979
TheGrayStone <nilslissner@gmx.de>
2020-06-05 12:41:48 +0200

    Completely refactored (and removed large parts of) the code  + primarily added frontend components

--- js/extension.js
+++ js/extension.js
@@ -2,33 +2,23 @@
 const s = document.createElement('SCRIPT');
 s.type = 'text/javascript';
 s.src = '/extensions/macrozilla/js/helpers.js';
+var macrosec = null;
 s.addEventListener('load', () => {
-  // add and hide all macrozilla-things
-  window.API.getThings().then(((things) => {
-    if (!Object.keys(things).find((x) => (window.basename(things[x].id) == 'macrozilla-variables')))
-      window.addThing('macrozilla-variables', 'Macrozilla Variables', 'Macrozilla Variables');
-  }).bind(this));
-  window.hideThing('macrozilla-variables');
-
   (function() {
     class MacrozillaExtension extends window.Extension {
 
       constructor() {
         super('macrozilla');
         this.addMenuEntry('Macros');
+        macrosec = document.querySelector("#extension-macrozilla-view").attachShadow({"mode": "closed"});
       }
 
       show() {
-        const path = document.location.hash === '' ? '' : document.location.hash.substr(1).replace(/-/g, '/');
-        import('/extensions/macrozilla/js/main.js')
-          .then((js) => {
-            const page = new js.default.prototype.constructor('main');
-            page.show(path);
-          })
-          .catch((e) => {
-            document.getElementById('extension-macrozilla-view').innerHTML = '<div class="extension-macrozilla-error">An internal error occured!</div>';
-            console.error(e);
-          });
+        window.loadPage("/extensions/macrozilla/pages/main.html").then((code) => {
+          macrosec.innerHTML = code;
+          parseMacrozillaComponent(macrosec);
+        });
+        
       }
 
     }
--- js/helpers.js
+++ js/helpers.js
@@ -1,93 +1,62 @@
 window.loadPage = function(page) {
-  return new Promise((resolve, reject) => {
-    fetch(page)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(res.statusText);
-        }
-        return res.text();
-      })
-      .then((text) => {
-        resolve(text);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
-
-window.showNotification = function(type, text, container, timeout = 0) {
-  const div = document.createElement('DIV');
-  div.className = `extension-macrozilla-${type}`;
-  div.innerHTML = text;
-  container.insertBefore(div, container.firstChild);
-  if (timeout) {
-    setTimeout(() => {
-      div.remove();
-    }, timeout*1000);
-  }
-};
-
-window.hideThing = function(id) {
-  const s = document.createElement('STYLE');
-  s.innerHTML = `#thing-${id}{display: none; pointer-events: none;}`;
-  document.body.appendChild(s);
-  console.log(id, 'macrozilla device hidden!');
-};
-
-window.addThing = function(id, title, description) {
-  window.API.postJson('/extensions/macrozilla/api/list-variables', {})
-    .then((res) => {
-      const props = {};
-      Object.keys(res).forEach((x) => {
-        props[x] = {type: res[x].datatype, value: res[x].value};
-      });
-      return window.API.addThing({id: id, title: title, description: description, properties: props});
-    })
-    .then(() => {
-      console.log(id, 'macrozilla device added!');
-    });
-};
-
-window.basename = function(path) {
-  try {
-    return path.split('/').reverse()[0];
-  } catch (ex) {
-    return false;
-  }
-};
-
-window.PageTemplate = class {
-
-  constructor(title) {
-    this.title = title;
-  }
-
-  show(path, container) {
     return new Promise((resolve, reject) => {
-      window.loadPage(`/extensions/macrozilla/views/${this.title}.html`)
-        .then((text) => {
-          document.getElementById(container).innerHTML = text;
-          if (path != '') {
-            const p = path.split('/');
-            const jslocation = `/extensions/macrozilla/js/${this.title}/${p[0]}.js`;
-            import(jslocation)
-              .then((js) => {
-                const page = new js.default.prototype.constructor(`${this.title}/${p[0]}`);
-                p.shift();
-                page.show(p.join('/'));
-                resolve();
-              })
-              .catch((ex) => {
-                reject(ex);
-              });
-          } else {
-            resolve();
-          }
+        fetch(page)
+        .then((res) => {
+            if (!res.ok) {
+            throw Error(res.statusText);
+            }
+            return res.text();
         })
-        .catch((ex) => {
-          reject(ex);
+        .then((text) => {
+            resolve(text);
+        })
+        .catch((err) => {
+            reject(err);
         });
     });
-  }
 };
+
+function parseMacrozillaComponent(parent){
+    parent.querySelectorAll(`*`).forEach((el) => {
+        if (el.tagName.substr(0,11) == "MACROZILLA-")
+            new MacrozillaPage(el.tagName.substr(11), el);
+        else if (el.tagName.substr(0,10) == "MACROCOMP-")
+            new MacrozillaElement(el.tagName.substr(10), el);
+    });
+}
+
+class MacrozillaComponent{
+    url;
+    nativeElement = null;
+    #shadowRoot = null;
+
+    constructor(elid, elmnt, url = ""){
+        this.url = url;
+        this.url += elid.toLowerCase();
+        this.nativeElement = elmnt;
+
+
+        window.loadPage(this.url+"/index.html").then((code) => {
+            this.parsePage(code);
+        });
+    }
+
+    parsePage = (html) => {
+        this.shadowRoot = this.nativeElement.attachShadow({"mode": "closed"});
+        this.shadowRoot.innerHTML = html;
+        this.shadowRoot.innerHTML += "<link rel='stylesheet' type='text/css' href='"+this.url+"/style.css'/>";
+        parseMacrozillaComponent(this.shadowRoot);
+    }
+}
+
+class MacrozillaPage extends MacrozillaComponent{
+    constructor(elid, elmnt){
+        super(elid, elmnt, "/extensions/macrozilla/pages/");
+    }
+}
+
+class MacrozillaElement extends MacrozillaComponent{
+    constructor(elid, elmnt){
+        super(elid, elmnt, "/extensions/macrozilla/components/");
+    }
+}
