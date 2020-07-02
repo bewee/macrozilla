223982abffe2c3805f2b9a8f203a9a2ff47c8b94
Galveston01 <benedikt.web23@gmail.com>
2020-07-02 21:44:46 +0200

    Add setter ability to fade to desired value

--- classes/setter/server.js
+++ classes/setter/server.js
@@ -16,7 +16,34 @@ class SetterClass {
     const right = description.right;
     assert(right && typeof right == 'object');
     assert(right.type && typeof right.type == 'string');
-    await this.handler.callClass(left.type, 'set', left, await this.handler.callClass(right.type, 'eval', right));
+
+    if ('speed' in description || 'time' in description) {
+      const srcval = await this.handler.callClass(left.type, 'eval', left);
+      const srcvalnum = this.handler.decodeNumber(srcval);
+      const dstval = await this.handler.callClass(right.type, 'eval', right);
+      const dstvalnum = this.handler.decodeNumber(dstval);
+      let time;
+      if ('speed' in description) {
+        const speed = await this.handler.callClass(description.speed.type, 'eval', description.speed);
+        time = Math.abs(dstvalnum-srcvalnum) / speed; // at speed 1, incrementing takes 1 second; at speed 10, incrementing takes 0.1 second
+      } else {
+        time = await this.handler.callClass(description.time.type, 'eval', description.time);
+      }
+      const ups = ('ups' in description) ? (await this.handler.callClass(description.ups.type, 'eval', description.ups)) : 5;
+      const stepnum = time * ups;
+      const stepsize = (dstvalnum-srcvalnum) / stepnum;
+      for (let i = 0; i < stepnum; i++) {
+        await this.handler.callClass(left.type, 'set', left, this.handler.encodeNumber(srcvalnum+stepsize*i));
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, 1000/ups);
+        });
+      }
+      await this.handler.callClass(left.type, 'set', left, this.handler.encodeNumber(dstvalnum));
+    } else {
+      await this.handler.callClass(left.type, 'set', left, await this.handler.callClass(right.type, 'eval', right));
+    }
   }
 
 }
