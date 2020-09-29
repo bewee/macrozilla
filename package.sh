14f577eec108501de93701c36b92919b19680e40
Galveston01 <44091658+Galveston01@users.noreply.github.com>
2020-09-29 21:42:43 +0200

    Add cross-compile github action (#4)
    * Add cross-compile github action
    Based on https://github.com/tim-hellhake/influxdb-bridge/commit/4ad0c70df297f4c49c4192975c46d94e03fcfd71
    
    * Fix builds. (#3)
    
    Co-authored-by: Michael Stegeman <michael@stegeman.me>

--- package.sh
+++ package.sh
@@ -1,22 +1,37 @@
 #!/bin/bash -e
 
+# Setup environment for building inside Dockerized toolchain
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
+[ $(id -u) = 0 ] && umask 0
+
 rm -rf node_modules
 
-npm install --production
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  NODE_VERSION="$(node --version)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
+fi
+
+npm ci --production
 
 shasum --algorithm 256 manifest.json package.json index.js LICENSE.md README.md > SHA256SUMS
-
-find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 find classes \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 find lib \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 find static \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 
+rm -rf node_modules/.bin
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
 TARFILE=`npm pack`
 
 tar xzf ${TARFILE}
+rm ${TARFILE}
+TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 cp -r node_modules ./package
-tar czf ${TARFILE} package
+tar czf ${TARFILE_ARCH} package
 
-shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+shasum --algorithm 256 ${TARFILE_ARCH} > ${TARFILE_ARCH}.sha256sum
 
 rm -rf SHA256SUMS package
