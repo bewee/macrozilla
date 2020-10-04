6dc98f89d522d71bdc6d18de6468e4e5c669fcf9
Benedikt Weber <benedikt.web23@gmail.com>
2020-10-05 00:15:04 +0200

    Add different text for variable trigger

--- classes/variable/client.js
+++ classes/variable/client.js
@@ -6,8 +6,7 @@
       const load_card = handler.addLoadCard(null, (copy) => {
         if (copy.internal_attributes.variable_id in this.variables) {
           // update text of newly loaded variable card
-          copy.setTooltipText(`Variable ${this.variables[copy.internal_attributes.variable_id]}`);
-          copy.setText(`${this.variables[copy.internal_attributes.variable_id]}`);
+          this.setupVariable(copy);
           copy.revive();
         } else {
           // mark card as unfinished loaded variable card
@@ -15,9 +14,6 @@
         }
       });
       load_card.setTooltipText(`Variable _/_`);
-      load_card.addAbility('evaluable');
-      load_card.addAbility('settable');
-      load_card.addAbility('trigger');
       load_card.setJSONAttribute('variable_id', null);
       load_card.setText('Variable(id=%a)', 'variable_id');
 
@@ -28,23 +24,27 @@
               this.variables[v.id] = `${p.name}/${v.name}`;
               // add variable cards to sidebar
               const card = handler.addCard(null, ['Variables']);
-              card.addAbility('evaluable');
-              card.addAbility('settable');
-              card.addAbility('trigger');
               card.setJSONAttribute('variable_id', v.id);
-              card.setTooltipText(`Variable ${this.variables[v.id]}`);
-              card.setText(`${this.variables[v.id]}`);
+              this.setupVariable(card);
               // update text of unfinished loaded variable cards
               document.querySelectorAll(`macro-card[variable-waiting='${v.id}']`).forEach((c) => {
                 c.removeAttribute('variable-waiting');
-                c.setTooltipText(`Variable $this.variables[v.id]}`);
-                c.setText(`${this.variables[v.id]}`);
+                this.setupVariable(c);
                 c.revive();
               });
             });
           });
         });
       });
+    }
+
+    setupVariable(card) {
+      card.addAbility('evaluable');
+      card.addAbility('settable');
+      card.addAbility('trigger', `${this.variables[card.internal_attributes.variable_id]} changed`);
+      card.setTooltipText(`Variable ${this.variables[card.internal_attributes.variable_id]}`);
+      card.setText(`${this.variables[card.internal_attributes.variable_id]}`);
+      card.refreshText();
     }
 
   }
