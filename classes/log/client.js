e2dea171e6a8b8e758686fdd8382b827d6a9ea15
Benedikt Weber <benedikt.web23@gmail.com>
2021-02-13 17:32:25 +0000

    Make info default log level of log block

--- classes/log/client.js
+++ classes/log/client.js
@@ -4,7 +4,7 @@
     constructor(handler) {
       const block = handler.addBlock(null, ['Basics']);
       block.setTooltipText('Log');
-      block.addInput('level', {type: 'string', enum: ['debug', 'info', 'warn', 'error', 'fatal'], venum: ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL']});
+      block.addInput('level', {type: 'string', enum: ['debug', 'info', 'warn', 'error', 'fatal'], venum: ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'], value: 'info'});
       block.addParameter('title', {accepts: 'evaluable'});
       block.addParameter('message', {accepts: 'evaluable'});
       block.setText('Log %i %p: %p', 'level', 'title', 'message');
